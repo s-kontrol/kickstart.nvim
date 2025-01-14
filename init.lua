53168994679925794c6f6863ced685b4afff2a76
s-kontrol <servicios.mruiz@gmail.com>
2025-01-14 12:23:46 -0300

    added icons

--- init.lua
+++ init.lua
@@ -315,7 +315,7 @@ require('lazy').setup {
         end,
       },
       { 'nvim-telescope/telescope-ui-select.nvim' },
-
+      { 'nvim-tree/nvim-web-devicons', opts = {} },
       -- Useful for getting pretty icons, but requires special font.
       --  If you already have a Nerd Font, or terminal set up with fallback fonts
       --  you can enable this
