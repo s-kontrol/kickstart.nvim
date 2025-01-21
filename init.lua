f39261df9a2ef23e74b0dd70d81a6d4959df1722
s-kontrol <servicios.mruiz@gmail.com>
2025-01-21 00:13:28 -0300

    color vim

--- init.lua
+++ init.lua
@@ -730,7 +730,7 @@ require('lazy').setup {
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- Load the colorscheme here
-      vim.cmd.colorscheme 'default'
+      vim.cmd.colorscheme 'vim'
 
       -- You can configure highlights by doing something like
       vim.cmd.hi 'Comment gui=none'
